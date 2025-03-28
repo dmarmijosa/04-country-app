c71e1075f41eb17e79c3af5c663264f4c76fd08f
Danny Armijos <daniel2667@hotmail.es>
2025-03-28 22:55:48 +0100

    agregar componente de pie de página y actualizar la estructura de la página de inicio

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent { }
