695428fdee760c0b6f93afdd81738b91e5bbe110
Danny Armijos <daniel2667@hotmail.es>
2025-03-29 18:27:10 +0100

    refactor: eliminar importación no utilizada en las rutas de país

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 
 export const countryRoutes: Routes = [
